6260c50ecf64dd6a943372a30e35399729c0ae80
fernanda binc <fernandacbincoletto@gmail.com>
2024-07-01 19:15:13 -0300

    Resolvendo desafio ListaTarefa

--- src/operacoes/java/ListaTarefa.java
+++ src/operacoes/java/ListaTarefa.java
@@ -1,2 +1,53 @@
-package operacoes.java;public class ListaTarefa {
+package operacoes.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    // atributo
+    private Lista <tarefa> tarefaLista;
+
+    public ListaTarefa() {
+        this.tarefaList =  new Arraylist<>();
+    }
+    public void adicionarTarefa(String descricao) {
+        tarefaLista.add(new.tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<tarefa> tarefasParaRemover = new ArrayList();
+        for(tarefa t : tarefaLista) {
+            if(t.getdescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+
+        }
+        tarefalist.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas(){
+        return tarefalist.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefalist);
+    }
+
+    public static void main(String[] args) {
+        listaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("o numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("tarefa 1");
+        listaTarefa.adicionarTarefa("tarefa 1");
+        listaTarefa.adicionarTarefa("tarefa 2");
+
+
+        listaTarefa.removerTarefa("Tarefa 2");
+        System.out.println("o numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas("");
+
+    }
+
 }
+
--- src/operacoes/java/Tarefa.java
+++ src/operacoes/java/Tarefa.java
@@ -1,2 +1,25 @@
-package operacoes.java;public class Tarefa {
+package operacoes.java;
+
+
+public class Tarefa {
+    //Atributo
+    private String descrição;
+
+    public Tarefa (String descrição){
+        this.descrição = descrição;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descrição='" + descrição + '\'' +
+                '}';
+    }
+
+
+
+
+
+
 }
